8f5972d4764f31f52a3b5093731d104018127070
KalininIA <snd64@bk.ru>
2020-06-10 01:01:52 +0300

    Task16

---
+++ src/Task16.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Task16 {
+    public static void main(String args[]) {
+        String s;
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader("C:/test/test.txt"))) {
+            while((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+        catch(IOException exc) {
+            System.out.println("Ошибка ввода-вывода: " + exc);
+        }
+    }
+}
